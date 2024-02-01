d0c4ae412e0836aaef6c7c90cd6ec89f75ced32c
Kook <jamesss075@gmail.com>
2024-02-01 00:15:52 -0800

    fixed a typo in task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,8 @@ def do_pack():
     date = datetime.now().strftime('%Y%m%d%H%M%S')
     archive_name = f'web_static_{date}.tgz'
     try:
-        local('mkdir -p version')
-        local(f'tar -czvf version/{archive_name} web_static')
+        local('mkdir -p versions')
+        local(f'tar -czvf versions/{archive_name} web_static')
         return f'versions/{archive_name}'
     except Exception:
         return None
