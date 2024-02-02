7fcd0ed5b123b8bfeef0c7e80fa8ebc4875a2321
Kook <jamesss075@gmail.com>
2024-02-02 00:53:21 -0800

    updated task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,32 @@
 #!/usr/bin/python3
 '''Fabric script that distributes an archive to my web servers'''
-from fabric.api import env, put, sudo
-from os.path import exists
-
+from fabric.api import env, put, sudo, run
+from os import path
 env.hosts = ['34.207.155.182', '18.204.14.80']
-
 
 
 def do_deploy(archive_path):
     '''Deploy func'''
-    if not exists(archive_path):
+    if not path.isfile(archive_path):
         return False
-    
-    # archive_path = /path/to/yor/archive.tar.gz
     try:
-        # Upload archive to /tmp/ dir on web server
-        put(archive_path, '/tmp/')
 
-        # gets you archive.tar.gz
-        filename = archive_path.split('/')[-1]
-        # gets you /path/to/your/
-        folder_name = filename.split('.')[0]
+        arch_tgz = archive_path.split("/")[-1]
+        arch = arch_tgz.split(".")[0]
+        # arch_dir = "/data/web_static/releases/{}/".format(arch)
+        arch_dir = '/data/web_static/releases/' + arch + '/'
 
-        release_path = '/data/web_static/releases/' + folder_name
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(arch_dir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(arch_tgz, arch_dir))
+        run("sudo rm /tmp/{}".format(arch_tgz))
+        run("sudo mv {}web_static/* {}".format(arch_dir, arch_dir))
+        run("sudo rm -rf {}web_static".format(arch_dir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(arch_dir))
 
-        sudo('mkdir -p {}'.format(release_path))
-
-        sudo('tar -xzf /tmp/{} -C {}'.format(filename, release_path))
-
-        # Delete archive from web server
-        sudo('rm /tmp/{}'.format(filename))
-
-        # delete the symbolic link /data/web_static/current
-        sudo('rm -f /data/web_static/current')
-
-
-        # Create a new symbolic link /data/web_static/current
-
-        sudo ('ln -s {} /data/web_static/current'.format(release_path))
-
+        print("New version deployed!")
         return True
-    
-    except Exception as e:
-        print(e)
+
+    except Exception:
         return False
