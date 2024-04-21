4e682330439f7996344ae633ef61c03797e321b8
Kok <kok@ubuntu.ubuntu-domain>
2024-04-21 21:19:35 +0300

    fixed logic of task 5 to show hmtl page if n is int

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,10 +7,12 @@ import sys
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     '''Display Hello HBNB!'''
     return ('Hello HBNB!')
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -24,6 +26,13 @@ def display_c_text(text):
     '''
     return "C {}".format(text.replace('_', ' '))
 
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    '''display “Python”, followed by the value of the text variable
+    '''
+    return "Python {}".format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
